ed6dbc9d1fb58cf1b4b0f9e497577944594d94af
TakoNya <camilabelentaraborelli2@gmail.com>
2019-11-19 20:59:33 -0300

    Primera versión

---
+++ src/sabores707/Sabores707.java
@@ -0,0 +1,12 @@
+package sabores707;
+
+public class Sabores707 {
+    
+    public static void main(String[] args) {
+        String[] sabores = {"Dulce", "Salado", "Amargo", "Acido", "Umami", "Picante", "Astringente", "Starchy", "Adiposo"};
+        for (int i = 0;i<6;i++){
+            System.out.println("Sabor: " + sabores[i]);
+        }
+    }
+    
+}
