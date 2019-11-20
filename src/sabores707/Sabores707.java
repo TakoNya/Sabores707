60dfff73eae8d1af02458e116899423e32f1ddca
TakoNya <camilabelentaraborelli2@gmail.com>
2019-11-19 21:22:04 -0300

    Se amplía la lista de sabores

--- src/sabores707/Sabores707.java
+++ src/sabores707/Sabores707.java
@@ -3,8 +3,8 @@ package sabores707;
 public class Sabores707 {
     
     public static void main(String[] args) {
-        String[] sabores = {"Dulce", "Salado", "Amargo", "Acido", "Umami", "Picante", "Astringente", "Starchy", "Adiposo"};
-        for (int i = 0;i<6;i++){
+        String[] sabores = {"Dulce", "NaCl", "Amargo", "Acido", "UmaUmaUHH", "UwU", "Starmyu", "CuSO4", "T-T"};
+        for (int i = 0;i<7;i++){
             System.out.println("Sabor: " + sabores[i]);
         }
     }
