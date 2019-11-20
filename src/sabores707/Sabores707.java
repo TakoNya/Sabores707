2e1760dcec1b82227dc0f03dc959b02e706bdfc5
b2 <b2@Ter72-W10.cen.local>
2019-11-19 21:16:49 -0300

    cambios sabores

--- src/sabores707/Sabores707.java
+++ src/sabores707/Sabores707.java
@@ -3,7 +3,7 @@ package sabores707;
 public class Sabores707 {
     
     public static void main(String[] args) {
-        String[] sabores = {"Dulce", "Salado", "Amargo", "Acido", "Umami", "Picante", "Astringente", "Starchy", "Adiposo"};
+        String[] sabores = {"Dulce", "Salado", "Amargo", "iara1", "Umami", "Picante", "iara2", "Starchy", "Adiposo"};
         for (int i = 0;i<6;i++){
             System.out.println("Sabor: " + sabores[i]);
         }
